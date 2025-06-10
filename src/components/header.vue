5711921d4b8afd4a35ba8bf1972c4e75cb1c90b9
ZHUZIOK <a1349089572@gmail.com>
2025-06-10 13:22:37 +0800

    fix:remove the header,vue sidebar

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,14 +2,14 @@
     <div class="navbar bg-base-100 shadow-sm">
         <div class="navbar-start">
             <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <!-- <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
-                </div>
-                <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                </div> -->
+                <!-- <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                     <li><a href="https://u3.com/">U3基金官网</a></li>
                     <li>
                         <a>U3</a>
@@ -26,12 +26,11 @@
                             <li><router-link to="/apayQalist"><a>Apay 常见问题</a></router-link></li>
                         </ul>
                     </li>
-                    <!-- <li><a @click="() => changeLang()">{{ t("message.switchLanguage") }}</a></li> -->
-                </ul>
+                </ul> -->
             </div>
-            <a class="btn btn-ghost text-xl">U3</a>
+            <a class="btn btn-ghost text-xl">Apay</a>
         </div>
-        <div class="navbar-center hidden lg:flex">
+        <!-- <div class="navbar-center hidden lg:flex">
             <ul class="menu menu-horizontal px-1">
                 <li><a href="https://u3.com/">U3基金官网</a></li>
                 <li>
@@ -49,9 +48,8 @@
                         <li><router-link to="/apayQalist"><a>Apay 常见问题</a></router-link></li>
                     </ul>
                 </li>
-                <!-- <li><a @click="() => changeLang()">{{ t("message.switchLanguage") }}</a></li> -->
             </ul>
-        </div>
+        </div> -->
         <div class="navbar-end ">
 
             <a @click="openNotionLink" class="btn" href="">{{ t("message.registrationAndLogin") }}</a>
